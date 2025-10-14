240a9f9e1e69bf9e47e6dcc3c5705643c18485e9
BadSnail11 <BadSnail11@github.com>
2025-10-14 11:41:23 +0300

    db upd

--- db/init/03_start.sql
+++ db/init/03_start.sql
@@ -1,2 +1,14 @@
 INSERT INTO user_bots (tg_bot_token, tg_bot_id, bot_username, is_super)
-VALUES ('8255159117:AAGSYPbLAtfFKhIlEQUvDf8swnFptujKXMc', '8255159117', '@stars_frag_bot', true); 
+VALUES ('8255159117:AAGSYPbLAtfFKhIlEQUvDf8swnFptujKXMc', '8255159117', '@stars_frag_bot', true);
+
+INSERT INTO pricing_rules (item_type, mode, manual_price, currency, bot_id)
+VALUES ('stars', 'manual', 1, 'RUB', 1);
+
+INSERT INTO pricing_rules (item_type, mode, manual_price, currency, bot_id)
+VALUES ('premium', 'manual', 1, 'RUB', 1);
+
+INSERT INTO pricing_rules (item_type, mode, manual_price, currency, bot_id)
+VALUES ('stars', 'manual', 1, 'TON', 1);
+
+INSERT INTO pricing_rules (item_type, mode, manual_price, currency, bot_id)
+VALUES ('premium', 'manual', 1, 'TON', 1);
