34ab35e1d86e233a7ee8040cdfe4313276710473
BadSnail11 <BadSnail11@github.com>
2025-10-14 11:18:04 +0300

    db upd

--- db/init/03_start.sql
+++ db/init/03_start.sql
@@ -1,2 +1,2 @@
 INSERT INTO user_bots (tg_bot_token, tg_bot_id, bot_username, is_super)
-VALUES ("8255159117:AAGSYPbLAtfFKhIlEQUvDf8swnFptujKXMc", "8255159117", "@stars_frag_bot", true); 
+VALUES ('8255159117:AAGSYPbLAtfFKhIlEQUvDf8swnFptujKXMc', '8255159117', '@stars_frag_bot', true); 
