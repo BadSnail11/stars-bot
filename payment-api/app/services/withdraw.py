04ef5942bf5905cfb174c399b20d666fd0c978e3
BadSnail11 <BadSnail11@github.com>
2025-10-18 18:56:22 +0300

    fix withdraw

--- payment-api/app/services/withdraw.py
+++ payment-api/app/services/withdraw.py
@@ -3,7 +3,11 @@
 
 import asyncio
 import os
-from TonTools import TonCenterClient, Wallet
+# from TonTools import TonCenterClient, Wallet
+from tonutils.wallet.data import WalletV5Data
+from tonutils.client.toncenter import ToncenterV2Client
+from tonutils.wallet import WalletV5R1
+
 
 # --- Настройки (замените на свои) ---
 TON_API_KEY = os.getenv("TON_API_KEY", "ВАШ_TONCENTER_API_KEY")
@@ -18,32 +22,30 @@ def _mnemonics() -> list[str]:
 
 async def create_withdraw_request(amount: float, address: str):
 
-    client = TonCenterClient(
-        key=TON_API_KEY,  # автоматически выберет URL TonCenter для тестнета/мейннета
-    )
+    client = ToncenterV2Client(api_key=TON_API_KEY)
+
+    wallet, public_key, private_key, mnemonic = WalletV5R1.from_mnemonic(client=client, mnemonic=_mnemonics())
 
     # Инициализация кошелька. Если версия не указана, TonTools попытается определить подходящую.
-    wallet = Wallet(
-        mnemonics=_mnemonics(),
-        address=TON_WALLET,
-        provider=client,
-        version=WALLET_VERSION,
+    
+    tx = await wallet.transfer(
+        destination=address,
+        amount=amount,
     )
-
     # Проверим адрес и баланс (необязательно)
     # balance = await wallet.get_balance()
     # print("Wallet address:", await wallet.)
-    balance_nano = await wallet.get_balance()  # в нанотонах
-    print("Current balance (nanoTON):", balance_nano)
+    # balance_nano = await wallet.get_balance()  # в нанотонах
+    # print("Current balance (nanoTON):", balance_nano)
 
-    # Отправка TON. TonTools сам получит seqno и сформирует сообщение.
-    tx = await wallet.transfer_ton(
-        destination_address=address,
-        amount=amount,   # в TON, не в nano
-        message=COMMENT,     # текстовый комментарий (опционально)
-        send_mode=3,         # pay gas separately; стандартный безопасный режим
-        # timeout=60           # сек — валидность сообщения (актуально для W5)
-    )
+    # # Отправка TON. TonTools сам получит seqno и сформирует сообщение.
+    # tx = await wallet.transfer_ton(
+    #     destination_address=address,
+    #     amount=amount,   # в TON, не в nano
+    #     message=COMMENT,     # текстовый комментарий (опционально)
+    #     send_mode=3,         # pay gas separately; стандартный безопасный режим
+    #     # timeout=60           # сек — валидность сообщения (актуально для W5)
+    # )
 
     return tx
     # В tx обычно возвращается словарь с информацией/хэшем отправки (может отличаться по версии)
